6ec1ac045e5b22fdf315ec4036633ee93840556f
aikhe <ikeandrie.ro@gmail.com>
2026-02-24 10:52:39 +0800

    feat(docs): getting though sveltekit documentations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,17 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	import { resolve } from '$app/paths';
+	import type { LayoutProps } from './$types';
+
+	let { children }: LayoutProps = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<nav>
+	<a href={resolve('/')}>root</a>
+	<a href={resolve('/scrutiny')}>scrutiny</a>
+</nav>
+
 {@render children()}

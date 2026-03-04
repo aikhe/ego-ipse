a0e66339464eaf9c9a95f162d1e6757333de3646
aikhe <ikeandrie.ro@gmail.com>
2026-03-04 18:42:28 +0800

    feat: 12 column grid overlay & layout via all sections

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,40 +11,95 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<header>
-	<div class="header-container container">
-		<img class="size-8" src={logo} alt="Aikhe Logo Mark" />
+<header class="section-container">
+	<img class="logo size-8" src={logo} alt="Aikhe Logo Mark" />
 
-		<nav class="container">
-			<a href={resolve('/')}>root</a>
-			<a href={resolve('/scrutiny')}>scrutiny</a>
-		</nav>
-	</div>
+	<p class="time">01:34.18 AM</p>
+
+	<p class="time-zone">GMT+8</p>
+
+	<p class="location">CALOOCAN, PH</p>
+
+	<nav class="nav">
+		<a href={resolve('/')}>ROOT</a>
+		<a href={resolve('/scrutiny')}>SCRUTINY</a>
+	</nav>
 </header>
 
 {@render children()}
+
+<div class="grid-overlay section-container">
+	{#each Array(12)}
+		<div class="grid-column"></div>
+	{/each}
+</div>
 
 <style>
 	header {
 		position: sticky;
 		top: 0;
-		z-index: 100;
-		display: flex;
-		justify-content: center;
+		z-index: 2;
+		height: 4.8rem;
+
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: 1.2rem;
 		align-items: center;
-		height: 4rem;
-		border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-		transition: transform 0.3s ease;
+		margin-inline: auto;
+		/* border-bottom: 1px solid rgba(255, 255, 255, 0.1); */
+
+		font-family: 'Geist Mono', monospace;
+		font-weight: 200;
+		letter-spacing: 0.34%;
+		font-size: 16px;
+		color: #797979;
 	}
 
-	.header-container {
+	.logo {
+		grid-column: 1;
+	}
+
+	.time {
+		grid-column: 2 / span 1;
+	}
+
+	.time-zone {
+		grid-column: 3;
+	}
+
+	.location {
+		grid-column: 4 / span 1;
+	}
+
+	.nav {
+		grid-column: 9 / span 2;
 		display: flex;
-		justify-content: space-between;
-		align-items: center;
+		gap: 1rem;
+		justify-content: flex-end;
 	}
 
 	nav {
 		display: flex;
 		gap: 1rem;
+	}
+
+	.grid-overlay {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		margin-inline: auto;
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: 1.2rem;
+		pointer-events: none;
+		z-index: 1;
+	}
+
+	.grid-column {
+		background-color: rgba(255, 255, 255, 0.04);
+		/* border-left: 1px solid rgba(255, 255, 255, 0.06); */
+		/* border-right: 1px solid rgba(255, 255, 255, 0.06); */
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,17 @@
-<section>
-	<div class="container">
-		<h1>Welcome to SvelteKit</h1>
-		<p>
-			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-		</p>
-	</div>
+<section class="section-container">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
 </section>
 
 <style>
 	section {
-		width: 100%;
-		display: flex;
-		justify-content: center;
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: 1.2rem;
 		align-items: center;
+		margin-inline: auto;
 		margin-top: 1.2rem;
 	}
 </style>

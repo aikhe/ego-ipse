55a8e62ab22fb97d2eebb3c329fca9e35cc1ee52
aikhe <ikeandrie.ro@gmail.com>
2026-03-09 01:07:23 +0800

    feat(deps): add @threlte/extras & first scene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,29 @@
 <script lang="ts">
-	// import Hero from '$lib/layouts/Hero.svelte';
-	// import Info from '$lib/layouts/Info.svelte';
+// import Hero from '$lib/layouts/Hero.svelte';
+// import Info from '$lib/layouts/Info.svelte';
 
-import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+import { Canvas } from '@threlte/core'
+import Scene from './Scene.svelte'
 </script>
 
-<section class="section-container">
+<Canvas>
+	<Scene />
+</Canvas>
 
-
-	<!-- <Hero /> -->
-	<!---->
-	<!-- <Info /> -->
-</section>
+<!-- <section class="section-container"> -->
+<!-- 	<Hero /> -->
+<!---->
+<!-- 	<Info /> -->
+<!-- </section> -->
 
 <style>
-	section {
-		height: calc(100vh - 4.8rem);
-
-		display: grid;
-		grid-template-columns: repeat(12, 1fr);
-		gap: 1.2rem;
-		align-items: end;
-		padding-bottom: 2rem;
-	}
+/* section { */
+/* 	height: calc(100vh - 4.8rem); */
+/**/
+/* 	display: grid; */
+/* 	grid-template-columns: repeat(12, 1fr); */
+/* 	gap: 1.2rem; */
+/* 	align-items: end; */
+/* 	padding-bottom: 2rem; */
+/* } */
 </style>

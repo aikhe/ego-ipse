9f769873b97b54bba0d000087cd067f6dcdbe5e9
aikhe <ikeandrie.ro@gmail.com>
2026-03-16 00:54:45 +0800

    feat(deps): three/threlte & @types/three + docs init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
-	import Hero from '$lib/layouts/Hero.svelte';
-	import Info from '$lib/layouts/Info.svelte';
+	// import Hero from '$lib/layouts/Hero.svelte';
+	// import Info from '$lib/layouts/Info.svelte';
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 </script>
 
 <section class="section-container">
-	<Hero />
 
-	<Info />
+
+	<!-- <Hero /> -->
+	<!---->
+	<!-- <Info /> -->
 </section>
 
 <style>

6f537b9c85c0d8668c7c0a0c9023fc0e293177ec
aikhe <ikeandrie.ro@gmail.com>
2026-04-04 16:49:32 +0800

    feat(stylelint): convert selectors & tailwind logic to ESM

---
+++ packages/stylelint-config/tailwind.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+/*
+ * Tailwind-specific at-rules
+ */
+export const CUSTOM_AT_RULES = [
+  // v4
+  'custom-variant',
+  'source',
+  'theme',
+  'plugin',
+  'utility',
+  'variant',
+  'reference',
+
+  // v3
+  'apply',
+  'config',
+  'layer',
+  'tailwind',
+
+  // v1, v2
+  'variants',
+  'responsive',
+  'screen',
+];

0f84a0108fe7af9b38de3f703430ab0a5b7125be
aikhe <ikeandrie.ro@gmail.com>
2026-04-04 17:13:54 +0800

    feat(web): stylelint initialization and setup

---
+++ stylelint.config.mjs
@@ -0,0 +1,40 @@
+import { ONLY_ALLOW_CAMEL_CASE_SELECTORS } from '@ego-ipse/stylelint-config/selectors';
+import { CUSTOM_AT_RULES } from '@ego-ipse/stylelint-config/tailwind';
+
+export default {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: [
+    '**/dist/**',
+    '**/build/**',
+    '**/.turbo/**',
+    '**/.svelte-kit/**',
+    '**/.wrangler/**',
+    '**/node_modules/**',
+  ],
+  rules: {
+    // Enforces Element Class Names to be camelCase
+    'selector-class-pattern': ONLY_ALLOW_CAMEL_CASE_SELECTORS,
+    // Enforces Element IDs to be camelCase
+    'selector-id-pattern': ONLY_ALLOW_CAMEL_CASE_SELECTORS,
+    // Allow Tailwind-based CSS Rules
+    'at-rule-no-unknown': [true, { ignoreAtRules: CUSTOM_AT_RULES }],
+    // Allow the Global CSS Selector
+    'selector-pseudo-class-no-unknown': [
+      true,
+      { ignorePseudoClasses: ['global'] },
+    ],
+    // Enforces the order of the CSS properties to be in alphabetical order
+    'order/properties-alphabetical-order': true,
+    // Enforces the order of the CSS properties to be in alphabetical order
+    'no-descending-specificity': null,
+    // Disables the Level-4 Media Queries; Since they're more exotic and less known
+    'media-feature-range-notation': 'prefix',
+    // Adopts the import notation from `postcss-import`
+    'import-notation': 'string',
+    // Allow the `@apply` at rule as its part of Tailwind
+    'at-rule-no-deprecated': [true, { ignoreAtRules: CUSTOM_AT_RULES }],
+
+    // 'nodejs/one-utility-class-per-line': true,
+  },
+};

56a19d355d5d3e22b7ac1ae02e0d1155bed25baa
aikhe <ikeandrie.ro@gmail.com>
2026-04-07 03:52:04 +0800

    feat(studio): sync shared eslint & typescript configs + turbo-aware linting & check-types scripts

--- apps/studio/eslint.config.mjs
+++ apps/studio/eslint.config.mjs
@@ -1,3 +1,4 @@
+import { config as baseConfig } from '@ego-ipse/eslint-config/base'
 import studio from '@sanity/eslint-config-studio'
 
-export default [...studio]
+export default [...baseConfig, ...studio]

e8fde5d744aaf35eaf0e5e5aca7a8ab27822377c
aikhe <ikeandrie.ro@gmail.com>
2026-07-07 20:30:49 +0800

    feat(studio): register social & github schemas

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,4 +1,6 @@
 import work from './work'
 import workRoot from './workRoot'
+import social from './social'
+import github from './github'
 
-export const schemaTypes = [work, workRoot]
+export const schemaTypes = [work, workRoot, social, github]

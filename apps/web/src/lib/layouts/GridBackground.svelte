3ea0e69894c227adfee42b4f78527c3b46bb58da
aikhe <ikeandrie.ro@gmail.com>
2026-07-11 15:14:02 +0800

    feat(web): extract dashed grid background into GridBackground component

---
+++ apps/web/src/lib/layouts/GridBackground.svelte
@@ -0,0 +1,36 @@
+<div class="grid-background section-container">
+  {#each Array.from(Array(12).keys()) as i (i)}
+    <div class="bg-grid-column"></div>
+  {/each}
+</div>
+
+<style>
+  .grid-background {
+    display: grid;
+    gap: 1.2rem;
+    grid-template-columns: repeat(12, 1fr);
+    inset: 0;
+    margin-inline: auto;
+    pointer-events: none;
+    position: absolute;
+    z-index: -99;
+  }
+
+  .bg-grid-column:is(
+    :nth-child(1),
+    :nth-child(2),
+    :nth-child(4),
+    :nth-child(7),
+    :nth-child(10)
+  ) {
+    border-left: 1px dashed var(--color-overlay-15);
+  }
+
+  .bg-grid-column:nth-child(9) {
+    border-right: 1px dashed var(--color-overlay-15);
+  }
+
+  .bg-grid-column:last-child {
+    border-right: 1px dashed var(--color-overlay-15);
+  }
+</style>

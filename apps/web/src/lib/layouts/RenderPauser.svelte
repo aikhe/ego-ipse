93cc8e750e06752c345e21be306fa6593313645e
aikhe <ikeandrie.ro@gmail.com>
2026-07-12 12:00:00 +0800

    feat(web): add RenderPauser component for toggling Canvas rendering

---
+++ apps/web/src/lib/layouts/RenderPauser.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { useThrelte } from '@threlte/core';
+
+  let { active }: { active: boolean } = $props();
+
+  const { autoRender } = useThrelte();
+
+  $effect(() => {
+    autoRender.set(active);
+  });
+</script>

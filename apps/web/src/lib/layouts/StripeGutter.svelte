972c3ac10db76062325a58a21cd99aeac3132869
aikhe <ikeandrie.ro@gmail.com>
2026-07-13 08:21:44 +0800

    refactor(web): strip inner stripe gutters from StripeGutter into root layout

--- apps/web/src/lib/layouts/StripeGutter.svelte
+++ apps/web/src/lib/layouts/StripeGutter.svelte
@@ -1,30 +1,7 @@
-<div class="stripe-gutter stripe-gutter--left"></div>
-<div class="stripe-gutter stripe-gutter--right"></div>
 <div class="stripe-gutter-outer stripe-gutter-outer--left"></div>
 <div class="stripe-gutter-outer stripe-gutter-outer--right"></div>
 
 <style>
-  .stripe-gutter {
-    background-color: var(--color-overlay-10);
-    bottom: 0;
-    mask-image: url('$lib/assets/stripe.svg');
-    mask-repeat: repeat;
-    mask-size: 7px 7px;
-    pointer-events: none;
-    position: absolute;
-    top: 0;
-    width: calc((100% - var(--container-width)) / 2);
-    z-index: -999;
-  }
-
-  .stripe-gutter--left {
-    left: 0;
-  }
-
-  .stripe-gutter--right {
-    right: 0;
-  }
-
   .stripe-gutter-outer {
     background-color: var(--color-overlay-10);
     bottom: 0;

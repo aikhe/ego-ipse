713157a2d6333d943a9b58c3f6262a3b8f7f9a3a
aikhe <ikeandrie.ro@gmail.com>
2026-07-16 12:08:00 +0800

    feat(grid): add corner tick marks to specific grid columns

--- apps/web/src/lib/layouts/GridBackground.svelte
+++ apps/web/src/lib/layouts/GridBackground.svelte
@@ -16,6 +16,10 @@
     z-index: -99;
   }
 
+  .bg-grid-column {
+    position: relative;
+  }
+
   .bg-grid-column:is(
     :nth-child(1),
     :nth-child(2),
@@ -32,5 +36,63 @@
 
   .bg-grid-column:last-child {
     border-right: 1px dashed var(--color-overlay-15);
+  }
+
+  .bg-grid-column:is(
+    :nth-child(1),
+    :nth-child(2),
+    :nth-child(4),
+    :nth-child(7),
+    :nth-child(10)
+  )::after {
+    background: var(--color-overlay-20);
+    content: '';
+    height: 10px;
+    left: -1.5px;
+    pointer-events: none;
+    position: absolute;
+    top: 4px;
+    width: 2px;
+  }
+
+  .bg-grid-column:nth-child(9)::after,
+  .bg-grid-column:last-child::after {
+    background: var(--color-overlay-20);
+    content: '';
+    height: 10px;
+    pointer-events: none;
+    position: absolute;
+    right: -1.5px;
+    top: 4px;
+    width: 2px;
+  }
+
+  .bg-grid-column:is(
+    :nth-child(1),
+    :nth-child(2),
+    :nth-child(4),
+    :nth-child(7),
+    :nth-child(10)
+  )::before {
+    background: var(--color-overlay-20);
+    bottom: 4px;
+    content: '';
+    height: 10px;
+    left: -1.5px;
+    pointer-events: none;
+    position: absolute;
+    width: 2px;
+  }
+
+  .bg-grid-column:nth-child(9)::before,
+  .bg-grid-column:last-child::before {
+    background: var(--color-overlay-20);
+    bottom: 4px;
+    content: '';
+    height: 10px;
+    pointer-events: none;
+    position: absolute;
+    right: -1.5px;
+    width: 2px;
   }
 </style>

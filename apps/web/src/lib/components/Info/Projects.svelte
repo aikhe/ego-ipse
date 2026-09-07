cb20c07c7ef5148dcd51ae992fa1db4efab3111a
aikhe <ikeandrie.ro@gmail.com>
2026-09-07 15:26:47 +0800

    refactor(projects): remove dwell & select tracking events

--- apps/web/src/lib/components/Info/Projects.svelte
+++ apps/web/src/lib/components/Info/Projects.svelte
@@ -4,7 +4,6 @@
   import { startGlitch } from '$lib/utils/glitch';
   import { toStageRect, toStageValue, toStageX } from '$lib/utils/stageScale';
   import type { Project } from '$lib/types/project';
-  import { getOpenPanel } from '$lib/analytics';
 
   import ProjectPreview from './ProjectPreview.svelte';
 
@@ -607,13 +606,6 @@
               opacity: 1,
               duration: 0.5,
             });
-            const dwellProject = projects[projectItems.indexOf(item)];
-            if (dwellProject) {
-              getOpenPanel()?.track('project_dwell', {
-                name: dwellProject.name,
-                id: dwellProject.id,
-              });
-            }
             const stillNotActive =
               !activeProject ||
               projects[projectItems.indexOf(item)]?.name !== activeProject.name;
@@ -655,10 +647,6 @@
         const project = projects[index];
         activeProject = project;
         onselect(project);
-        getOpenPanel()?.track('project_select', {
-          name: project.name,
-          id: project.id,
-        });
 
         // Glitch the project name on click
         if (glitchIntervals[index]) clearInterval(glitchIntervals[index]!);

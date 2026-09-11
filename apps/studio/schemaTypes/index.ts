d6b4cca8bf8dcf25abd8e83fdad0ec0dad2ed5ad
aikhe <ikeandrie.ro@gmail.com>
2026-09-11 11:00:00 +0800

    chore(studio): register opusStack type in schema index

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import workRoot from './workRoot'
 import opusWork from './opusWork'
 import opusValues from './opusValues'
 import opusAbout from './opusAbout'
+import opusStack from './opusStack'
 import social from './social'
 import github from './github'
 
-export const schemaTypes = [work, workRoot, opusWork, opusValues, opusAbout, social, github]
+export const schemaTypes = [work, workRoot, opusWork, opusValues, opusAbout, opusStack, social, github]
